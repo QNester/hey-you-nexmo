ee052761f1f2d1a7173afbab3203e0dd90f724af
Sergey Nesterov <qnesterr@gmail.com>
2020-07-01 11:57:19 +0300

    Init commit

---
+++ lib/hey/you/nexmo.rb
@@ -0,0 +1,10 @@
+require "hey/you/nexmo/version"
+
+module Hey
+  module You
+    module Nexmo
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/hey/you/nexmo/version.rb
@@ -0,0 +1,7 @@
+module Hey
+  module You
+    module Nexmo
+      VERSION = "0.1.0"
+    end
+  end
+end
