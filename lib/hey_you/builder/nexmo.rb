04795a92c6feac0e2e75afd50a540091a93656cb
Sergey Nesterov <qnesterr@gmail.com>
2020-07-03 19:42:33 +0300

    Fix text interpolation in builder

--- lib/hey_you/builder/nexmo.rb
+++ lib/hey_you/builder/nexmo.rb
@@ -4,7 +4,7 @@ module HeyYou
       attr_reader :text, :from, :is_unicode
 
       def build
-        @text = ch_data.fetch('text')
+        @text = interpolate(ch_data.fetch('text'), options)
         @from = ch_data.fetch('from', nil)
         @is_unicode = ch_data.fetch('is_unicode', false)
       rescue KeyError => e
